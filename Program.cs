1e0937f0a24184618b3aa4ad3138d23b6f52fe41
pedrosqr1 <127567823+pedrosqr1@users.noreply.github.com>
2023-03-10 20:29:33 -0300

    Add files via upload
    Atividade Programação 1 - Exercícios de revisão em C# (Vale Nota)
    Atividade Programação 1 - Exercícios de revisão em C# (Vale Nota de Atividade)
    Entregar preferencialmente Link De Repositório Público (sugestão GitHub)
    Entregar Pdf ou Doc Com todos os Fontes (Somente copiar para o Documento o conteúdo dos arquivos  .cs)

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿static void Main(string[] args)
+{
+    //Criando lista
+    List<string> ListaFrutas = new List<string>;
+    string entrada;
+
+    Console.WriteLine("Entre com A Lista Conforme Manual do Sistema:");
+    //Ex entrada:banana;maca;laranja;laranja"
+    entrada = Console.ReadLine();
+
+    //Pega Array de palavras separados por ';'
+    string[] strArray = entrada.Split(';');
+
+    //Para cada Palavra no array
+    foreach (string str in strArray)
+    {
+
+        //Faz uma copia da string em Lower Case
+        string fruta = str.ToLower();
+
+        //Copia a primeira Letra da string em UpperCase
+        string primeiraLetra = fruta.Substring(0, 1).ToUpper();
+
+        //fruta recebe PrimeiraLetra maiúscula e restante minúsculo;
+        fruta = primeiraLetra
+            + fruta.Substring(1, fruta.Length - 1);
+
+        //se a fruta ainda não existir na lista adiciona
+        if (!ListaFrutas.Contains(fruta))
+            ListaFrutas.Add(fruta);
+    }
+
+    Console.WriteLine("Resposta:" + Environment.NewLine);
+    //Exibi Lista de Frutas
+    foreach (string fruta in ListaFrutas)
+        Console.WriteLine(fruta);
